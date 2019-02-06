bb10991f74e5226249f9b35b8c29ad22a5514afa
Jason R. Coombs <jaraco@jaraco.com>
2019-02-06 16:08:59 -0500

    Add test for list objects

--- test_treehouse.py
+++ test_treehouse.py
@@ -22,3 +22,10 @@ def test_basic_transform():
 
 	new_ob = treehouse.map_values(make_decimal, ob)
 	assert new_ob['a'] == decimal.Decimal('1.0')
+
+
+def test_list_transform():
+	ob = ['a', 'b', 'c']
+	new_ob = treehouse.map_values(make_decimal, ob)
+	assert new_ob == ob
+	assert new_ob is not ob
