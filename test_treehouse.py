481bd3c98d53e52cf3c9dbbb481fc7315f1a67c5
Jason R. Coombs <jaraco@jaraco.com>
2019-02-06 15:57:17 -0500

    Add basic test

---
+++ test_treehouse.py
@@ -0,0 +1,24 @@
+import decimal
+
+import treehouse
+
+
+def make_decimal(ob):
+	try:
+		if ob['_type'] == 'decimal':
+			return decimal.Decimal(ob['_val'])
+	except Exception:
+		pass
+	return ob
+
+
+def test_basic_transform():
+	ob = dict(
+		a=dict(
+			_type='decimal',
+			_val='1.0',
+		),
+	)
+
+	new_ob = treehouse.map_values(make_decimal, ob)
+	assert new_ob['a'] == decimal.Decimal('1.0')
