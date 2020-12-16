aa9ab47b9c2bb7ab44e9f74d05edf39a3ab3bf34
Jason R. Coombs <jaraco@jaraco.com>
2020-12-15 22:30:47 -0500

    ⚫ Fade to black.

--- test_treehouse.py
+++ test_treehouse.py
@@ -4,23 +4,23 @@ import treehouse
 
 
 def make_decimal(ob):
-	return decimal.Decimal(ob['_val']) if ob['_type'] == 'decimal' else ob
+    return decimal.Decimal(ob['_val']) if ob['_type'] == 'decimal' else ob
 
 
 def test_basic_transform():
-	ob = dict(
-		a=dict(
-			_type='decimal',
-			_val='1.0',
-		),
-	)
+    ob = dict(
+        a=dict(
+            _type='decimal',
+            _val='1.0',
+        ),
+    )
 
-	new_ob = treehouse.map_values(make_decimal, ob)
-	assert new_ob['a'] == decimal.Decimal('1.0')
+    new_ob = treehouse.map_values(make_decimal, ob)
+    assert new_ob['a'] == decimal.Decimal('1.0')
 
 
 def test_list_transform():
-	ob = ['a', 'b', 'c']
-	new_ob = treehouse.map_values(make_decimal, ob)
-	assert new_ob == ob
-	assert new_ob is not ob
+    ob = ['a', 'b', 'c']
+    new_ob = treehouse.map_values(make_decimal, ob)
+    assert new_ob == ob
+    assert new_ob is not ob
